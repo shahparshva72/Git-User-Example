9ace74f742ab07b4dae13cd09b03eb98f5c9ad0f
CSPIT-IT <cspit-it@cspit-its-imac.local>
2018-08-04 12:44:27 +0530

    Log message on App Launch Add Button to UI that logs to console when tapped

--- My First App/AppDelegate.swift
+++ My First App/AppDelegate.swift
@@ -14,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    print("Application did finish launching")
+    return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
