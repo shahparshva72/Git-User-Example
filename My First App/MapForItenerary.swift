1d7fd3285ab64c7130e7f505bd47d9854be50457
CSPIT-IT <cspit-it@cspit-its-imac.local>
2018-08-04 12:53:09 +0530

    Add new Map-for-itenerary feature

---
+++ My First App/MapForItenerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.swift
+//  My First App
+//
+//  Created by CSPIT-IT on 04/08/18.
+//  Copyright © 2018 Parshva. All rights reserved.
+//
+
+import Foundation
+
+class MapForItenerary: NSObject{
+    
+}
